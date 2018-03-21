19656fe943bc0e2c1514d44bc1ed090c03054fe5
lise <lise.p.poirier@gmail.com>
2018-03-21 15:59:05 +0100

    html ok

--- public/javascripts/rhtest.js
+++ public/javascripts/rhtest.js
@@ -1,58 +1,57 @@
 /**
  * Created by bjornberthelot on 29/05/15.
  */
-var app = angular.module("rhtest", []);
+var imageArray = [{
+	"name": "image1.jpg",
+	"width": 500,
+	"height": 375
+	}, {
+	"name": "image2.jpg",
+	"width": 2048,
+	"height": 1280
+	}, {
+	"name": "image3.jpeg",
+	"width": 275,
+	"height": 183
+	}, {
+	"name": "image4.jpeg",
+	"width": 275,
+	"height": 183
+	}, {
+	"name": "image5.jpeg",
+	"width": 344,
+	"height": 147
+	}, {
+	"name": "image6.jpeg",
+	"width": 260,
+	"height": 194
+	}, {
+	"name": "images.jpeg",
+	"width": 194,
+	"height": 259
+	}, {
+	"name": "ole.jpg",
+	"width": 677,
+	"height": 788
+	}, {
+	"name": "telechargement.jpeg",
+	"width": 259,
+	"height": 194
+	}];
 
+var app = angular.module("rhtest", []);
 app.controller("rhCtrl", ['$scope', 'ImageService', '$http', function ($scope, ImageService, $http) {
 	$scope.campaign = {
 		dateLimit: true
 	};
-	
-	//////////// Changement de "null" par une image pour avoir un affichage
+
+	//////////////////////// Changement de "null" par une image pour avoir un affichage
 	$scope.currentImage = "image1.jpg";
-	////////////
-	
-	var imageArray = [{
-		"name": "image1.jpg",
-		"width": 500,
-		"height": 375
-	}, {
-		"name": "image2.jpg",
-		"width": 2048,
-		"height": 1280
-	}, {
-		"name": "image3.jpeg",
-		"width": 275,
-		"height": 183
-	}, {
-		"name": "image4.jpeg",
-		"width": 275,
-		"height": 183
-	}, {
-		"name": "image5.jpeg",
-		"width": 344,
-		"height": 147
-	}, {
-		"name": "image6.jpeg",
-		"width": 260,
-		"height": 194
-	}, {
-		"name": "images.jpeg",
-		"width": 194,
-		"height": 259
-	}, {
-		"name": "ole.jpg",
-		"width": 677,
-		"height": 788
-	}, {
-		"name": "telechargement.jpeg",
-		"width": 259,
-		"height": 194
-	}];
-	
+	////////////////////////
+
 	$scope.images = imageArray;
 	$scope.imageSelection = [];
-	
+
 	ImageService.load().then(function (data) {
 		$scope.images = data;
 	});
@@ -64,57 +63,20 @@ app.controller("rhCtrl", ['$scope', 'ImageService', '$http', function ($scope, I
 	$scope.addItem = function (image) {
 		$scope.imageSelection.push(image);
 	};
-	
-	//////////// Mon code
+
+	//////////////////////// Mon code
 	$http.get("/imagesapi").
 	success(function (data, status) {
 		$scope.imageSelection = data;
 	});
-	////////////
+	////////////////////////
 }]);
 
+//////////////////////// REPONSE À LA QUESTION 3 ANGULAR
 app.controller("reponse3", ['$scope', function ($scope) {
-	var imageArray = [{
-		"name": "image1.jpg",
-		"width": 500,
-		"height": 375
-	}, {
-		"name": "image2.jpg",
-		"width": 2048,
-		"height": 1280
-	}, {
-		"name": "image3.jpeg",
-		"width": 275,
-		"height": 183
-	}, {
-		"name": "image4.jpeg",
-		"width": 275,
-		"height": 183
-	}, {
-		"name": "image5.jpeg",
-		"width": 344,
-		"height": 147
-	}, {
-		"name": "image6.jpeg",
-		"width": 260,
-		"height": 194
-	}, {
-		"name": "images.jpeg",
-		"width": 194,
-		"height": 259
-	}, {
-		"name": "ole.jpg",
-		"width": 677,
-		"height": 788
-	}, {
-		"name": "telechargement.jpeg",
-		"width": 259,
-		"height": 194
-	}];
-	
 	$scope.images = imageArray;
 }]);
-
+////////////////////////
 
 app.factory('ImageService', ['$http', function ($http) {
 	var imageService = {
